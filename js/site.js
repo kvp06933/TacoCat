c5f8507dff0fb997812ae5e556715b02aa1bebb7
Katherine Pitts <kvp06933@gmail.com>
2021-08-18 18:57:41 -0500

    Added basic UI and mapped out functions

--- js/site.js
+++ js/site.js
@@ -1,4 +1,28 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello world");
+//get values -- controller function
+function getValue(){
+    let userInput = document.getElementById("userInput");
+    let tacoCat = tacoCat(usrString);
+    isPalindrome(tacoCatRev);
+}
+
+
+//tacoCat -- logic function
+function tacoCat(input){
+    let tacoCatRev = [];
+    for(i=input; i>= 0; i--){
+        tacoCatRev.push(i);
+    }
+    return tacoCatRev;
+}
+//isPalindrome -- view function
+function isPalindrome(tacoCatRev){
+    let alert = document.getElementById("isPalindromeDisplay");
+    if(tacoCatRev === input){
+        alert.innerHTML = `${tacoCatRev} is a palindrome!`;
+        alert.classList.add("alert-success");    
+    } else {
+        alert.innerHTML = `${tacoCatRev} is <strong>NOT</strong> a palindrome`;
+    }
+
+    
 }
