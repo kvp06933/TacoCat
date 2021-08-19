49f58ea49db76448dac2dc85d6a3f80a5db72b2c
Katherine Pitts <kvp06933@gmail.com>
2021-08-19 11:20:38 -0500

    added display javascript, removed special characters, converted to lowercase

--- js/site.js
+++ js/site.js
@@ -1,27 +1,42 @@
 //get values -- controller function
 function getValue(){
-    let userInput = document.getElementById("userInput");
-    let tacoCat = tacoCat(usrString);
-    isPalindrome(tacoCatRev);
+    document.getElementById("isPalindromeDisplay").classList.add("invisible");
+    
+    let str = document.getElementById("userInput").value;
+    let strRegex = str.replace(/[^a-zA-Z]/g, "");
+    let strLowerCase = strRegex.toLowerCase();
+    if(strLowerCase.value == ""){
+        alert("Please enter a string");
+    } else{
+        let tacoCatRev = tacoCat(strLowerCase);
+        isPalindrome(tacoCatRev, strLowerCase);
+    }
+    
+
 }
 
 
 //tacoCat -- logic function
-function tacoCat(input){
+function tacoCat(str){
     let tacoCatRev = [];
-    for(i=input; i>= 0; i--){
-        tacoCatRev.push(i);
+    for(i=str.length-1; i>= 0; i--){
+        tacoCatRev += str[i];
     }
     return tacoCatRev;
 }
 //isPalindrome -- view function
-function isPalindrome(tacoCatRev){
+function isPalindrome(tacoCatRev, str){
     let alert = document.getElementById("isPalindromeDisplay");
-    if(tacoCatRev === input){
+    alert.classList.remove("invisible", "alert-success", "alert-danger");
+    alert.classList.add("show");
+    if(tacoCatRev === str){
         alert.innerHTML = `${tacoCatRev} is a palindrome!`;
+        
         alert.classList.add("alert-success");    
     } else {
         alert.innerHTML = `${tacoCatRev} is <strong>NOT</strong> a palindrome`;
+        
+        alert.classList.add("alert-danger");
     }
 
     
